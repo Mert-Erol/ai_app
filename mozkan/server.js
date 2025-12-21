7637221b87fd5a36452094017ca9a7f6cff44f04
Mustafa Özkan <leongrphc@gmail.com>
2025-12-21 09:58:05 +0300

    tasarım tamamlandı, gitignore eklendi

---
+++ mozkan/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 4141;
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
+});
